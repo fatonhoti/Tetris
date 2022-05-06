c8723d7e28c9366ba26f452209aec98b0ea98ef0
Faton Hoti <hotifaton@outlook.com>
2022-05-06 20:20:35 +0200

    Testing drawing stuff on the screen

--- Tetris.py
+++ Tetris.py
@@ -12,13 +12,25 @@ BLOCK_HEIGHT = BLOCK_WIDTH
 SCREEN_HEIGHT = BLOCK_HEIGHT * GRID_HEIGHT
 SCREEN_WIDTH = BLOCK_WIDTH * GRID_WIDTH
 
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+PURPLE = (255, 0, 255)
+ORANGE = (255, 165, 0)
+TURQUOISE = (64, 224, 208)
+GRAY = (128, 128, 128)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 class Tetris:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.grid = [[0] * GRID_WIDTH for _ in range(GRID_HEIGHT)]
-        self.print_grid()
+        self.grid[0][0] = 1
+        self.grid[0][2] = 1
+        self.grid[0][4] = 1
+        self.draw()
 
     def run(self):
         running = True
@@ -27,6 +39,20 @@ class Tetris:
                 if event.type == pygame.QUIT:
                     running = False
                     pygame.quit()
+    
+    def draw(self):
+        # Draw tetrominos
+        for r, row in enumerate(self.grid):
+            for c, block in enumerate(row):
+                # Background block
+                pygame.draw.rect(self.screen, BLACK, (c * BLOCK_WIDTH, r * BLOCK_HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT))
+                if block:
+                    # Tetromino block
+                    pygame.draw.rect(self.screen, ORANGE, (c * BLOCK_WIDTH, r * BLOCK_HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT))
+        # Draw falling tetromino
+
+        # Update screen
+        pygame.display.flip()
 
     def print_grid(self):
         """For debugging only"""
